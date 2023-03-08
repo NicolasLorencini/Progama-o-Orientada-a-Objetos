c2ddd8b0f7eb2f21c04e406624b1b51520ab111a
NicolasLorencini <126730205+NicolasLorencini@users.noreply.github.com>
2023-03-08 19:35:39 -0300

    Add files via upload

---
+++ Atirbutosanimal/src/atirbutosanimal/AtributosAnimal.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atirbutosanimal;
+
+/**
+ *
+ * @author aluno
+ */
+public class AtributosAnimal {
+    
+    String cor;
+    String corOlhos;
+    String sexo;
+    double peso;
+    int idade;
+
+    public static void main(String[] args) {
+       
+       
+   
+        
+        AtributosAnimal aurora = new AtributosAnimal();
+        aurora.cor = "Azul";
+        aurora.corOlhos = "Preto";
+        aurora.sexo = "F";
+        aurora.peso = 1.5;
+        aurora.idade = 10;
+        
+        AtributosAnimal cris = new AtributosAnimal();
+        cris.cor = "Preto";
+        cris.corOlhos = "Azul";
+        cris.sexo = "M";
+        cris.peso = 110;
+        cris.idade = 5;
+        
+        AtributosAnimal marcos = new AtributosAnimal();
+        marcos.cor = "Verde";
+        marcos.corOlhos = "Preto";
+        marcos.sexo = "M";
+        marcos.peso = 70;
+        marcos.idade = 7;
+        
+        AtributosAnimal cristina = new AtributosAnimal();
+        cristina.cor = "Vermelho";
+        cristina.corOlhos = "Preto";
+        cristina.sexo = "F";
+        cristina.peso = 1.0;
+        cristina.idade = 13;
+        
+        AtributosAnimal punpun = new AtributosAnimal();
+        punpun.cor = "Laranja e preto";
+        punpun.corOlhos = "Preto";
+        punpun.sexo = "F";
+        punpun.peso = 65;
+        punpun.idade = 1;
+        
+    }
+}
+
