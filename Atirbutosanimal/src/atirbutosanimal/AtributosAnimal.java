93677f683ce831e13e7fb06e5887e3e6f0df9f1b
NicolasLorencini <126730205+NicolasLorencini@users.noreply.github.com>
2023-03-08 20:03:04 -0300

    Add files via upload

---
+++ Atirbutosanimal/src/atirbutosanimal/AtributosAnimal.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atirbutosanimal;
+
+/**
+ *
+ * @author aluno
+ */
+
+//Criar uma classe distinguindo 5 atributos para definir um animal. E depois criar uma outra classe para cadastrar 5 animais.
+
+public class AtributosAnimal {
+    
+
+    public static void main(String[] args) {
+       
+       
+   
+        
+        Separacao aurora = new Separacao();
+        aurora.cor = "Azul";
+        aurora.corOlhos = "Preto";
+        aurora.sexo = "F";
+        aurora.peso = 1.5;
+        aurora.idade = 10;
+        
+        Separacao cris = new Separacao();
+        cris.cor = "Preto";
+        cris.corOlhos = "Azul";
+        cris.sexo = "M";
+        cris.peso = 110;
+        cris.idade = 5;
+        
+        Separacao marcos = new Separacao();
+        marcos.cor = "Verde";
+        marcos.corOlhos = "Preto";
+        marcos.sexo = "M";
+        marcos.peso = 70;
+        marcos.idade = 7;
+        
+        Separacao cristina = new Separacao();
+        cristina.cor = "Vermelho";
+        cristina.corOlhos = "Preto";
+        cristina.sexo = "F";
+        cristina.peso = 1.0;
+        cristina.idade = 13;
+        
+        Separacao punpun = new Separacao();
+        punpun.cor = "Laranja e preto";
+        punpun.corOlhos = "Preto";
+        punpun.sexo = "F";
+        punpun.peso = 65;
+        punpun.idade = 1;
+        
+    }
+}
+
---
+++ Atirbutosanimal/src/atirbutosanimal/Separacao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atirbutosanimal;
+
+/**
+ *
+ * @author aluno
+ */
+public class Separacao {
+    public String cor;
+   public String corOlhos;
+   public String sexo;
+   public double peso;
+    public int idade;
+}
